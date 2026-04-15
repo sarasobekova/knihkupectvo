b63c3421785c8664ea4a4ded6f697f28c8b2ee6b
saras <sarasobekova@azet.sk>
2026-04-15 12:26:33 +0200

    Pridanie podstránky knihy.php
    Pridanie základu podstránky, ktorá sa bude neskôr ešte upravovať

---
+++ knihkupectvo/knihy.php
@@ -0,0 +1,34 @@
+<?php include 'header.php'; ?>
+
+<h2>📚 V ponuke</h2>
+    <link rel="stylesheet" href="style.css">
+
+<div class="container">
+
+    <div class="book">
+        <h3>Harry Potter</h3>
+        <p>Autor: J.K. Rowling</p>
+        <p>Magický príbeh o čarodejníkovi.</p>
+        <p class="price">12.99€</p>
+        <button onclick="alert('Pridané do košíka')">Kúpiť</button>
+    </div>
+
+    <div class="book">
+        <h3>Pán prsteňov</h3>
+        <p>Autor: J.R.R. Tolkien</p>
+        <p>Epické fantasy dobrodružstvo.</p>
+        <p class="price">15.99€</p>
+        <button onclick="alert('Pridané do košíka')">Kúpiť</button>
+    </div>
+
+    <div class="book">
+        <h3>1984</h3>
+        <p>Autor: George Orwell</p>
+        <p>Dystopický román o totalite.</p>
+        <p class="price">10.50€</p>
+        <button onclick="alert('Pridané do košíka')">Kúpiť</button>
+    </div>
+
+</div>
+
+<?php include 'footer.php'; ?>
